50469648ad5220c114f6d270244823b8660e0e73
Charles Sy <charles.m.sy@hotmail.com>
2020-06-09 17:51:56 -0700

    * Employee.cs: * ProgramData.cs: * EmployeeProgram.exe: * EmployeeProgram.pdb: * EmployeeProgram.exe: * EmployeeProgram.pdb: * EmployeeProgramHeapSort.csproj: * EmployeeProgram.csprojAssemblyReference.cache: * EmployeeProgram.csproj.CoreCompileInputs.cache:
    * Program.cs: Updated code. Removed ProgramData.cs and added static
      methods. Also Min and Max sort combined together.

--- EmployeeProgram/Program.cs
+++ EmployeeProgram/Program.cs
@@ -4,14 +4,132 @@
 
 using System;
 
-namespace EmployeeProgram
+namespace EmployeeProgramHeapSort
 {
     class MainClass
     {
+        public static void Sort(bool isMaxheap)
+        {
+            int lengthEffective = EmployeeList.Length;
+
+            if (isMaxheap)
+            {
+                // Build heap
+                for (int i = lengthEffective / 2 - 1; i >= 0; i--)
+                    Heapify(EmployeeList, lengthEffective, i, isMaxheap);
+
+                // The actual sorting process 
+                for (int i = lengthEffective - 1; i > 0; i--)
+                {
+                    // Move current root to end 
+                    Employee temp = EmployeeList[0];
+                    EmployeeList[0] = EmployeeList[i];
+                    EmployeeList[i] = temp;
+
+                    // call max heapify on the reduced heap 
+                    Heapify(EmployeeList, i, 0, isMaxheap);
+                }
+            }
+
+            else
+            {
+                // Build heap
+                for (int i = lengthEffective / 2 - 1; i >= 0; i--)
+                    Heapify(EmployeeList, lengthEffective, i, isMaxheap);
+
+                // The actual sorting process 
+                for (int i = lengthEffective - 1; i > 0; i--)
+                {
+                    // Move current root to end 
+                    Employee temp = EmployeeList[0];
+                    EmployeeList[0] = EmployeeList[i];
+                    EmployeeList[i] = temp;
+
+                    // call min heapify on the reduced heap 
+                    Heapify(EmployeeList, i, 0, isMaxheap);
+                }
+
+            }
+            PrintArray();
+        }
+
+        public static void Heapify(Employee[] _EmployeeList, int lengthEffective, int i, bool isMaxheap)
+        {
+            int smallestOrLargest = i;
+            int l = 2 * i + 1;
+            int r = 2 * i + 2;
+
+            if (isMaxheap)
+            {
+                // If left child is larger than root
+                if (l < lengthEffective && _EmployeeList[l].getIdNumber() > _EmployeeList[smallestOrLargest].getIdNumber())
+                    smallestOrLargest = l;
+
+                // If right child is larger than largest so far 
+                if (r < lengthEffective && _EmployeeList[r].getIdNumber() > _EmployeeList[smallestOrLargest].getIdNumber())
+                    smallestOrLargest = r;
+
+                // If largest is not root 
+                if (smallestOrLargest != i)
+                {
+                    Employee swap = _EmployeeList[i];
+                    _EmployeeList[i] = _EmployeeList[smallestOrLargest];
+                    _EmployeeList[smallestOrLargest] = swap;
+
+                    // Recursively heapify the affected sub-tree 
+                    Heapify(_EmployeeList, lengthEffective, smallestOrLargest, isMaxheap);
+                }
+            }
+
+            else
+            {
+                // If left child is less than root
+                if (l < lengthEffective && _EmployeeList[l].getIdNumber() < _EmployeeList[smallestOrLargest].getIdNumber())
+                    smallestOrLargest = l;
+
+                // If right child is less than largest so far 
+                if (r < lengthEffective && _EmployeeList[r].getIdNumber() < _EmployeeList[smallestOrLargest].getIdNumber())
+                    smallestOrLargest = r;
+
+                // If lowest is not root 
+                if (smallestOrLargest != i)
+                {
+                    Employee swap = _EmployeeList[i];
+                    _EmployeeList[i] = _EmployeeList[smallestOrLargest];
+                    _EmployeeList[smallestOrLargest] = swap;
+
+                    // Recursively heapify the affected sub-tree 
+                    Heapify(_EmployeeList, lengthEffective, smallestOrLargest, isMaxheap);
+                }
+            }
+        }
+
+        public static void PrintArray() {
+            int n = EmployeeList.Length;
+            Console.WriteLine("Listing employees");
+            Console.WriteLine("");
+            Console.WriteLine("ID         NAME");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("{1}       {0}", EmployeeList[i].getName(), EmployeeList[i].getIdNumber());
+            }
+            Console.WriteLine("");
+        }
+
+        private static Employee[] EmployeeList;
+
         public static void Main(string[] args)
         {
             // Data //
-            ProgramData data = new ProgramData();
+            EmployeeList = new [] {
+                new Employee("Office_Lady", 1015),
+                new Employee("Desk_Gentleman", 1050),
+                new Employee("NEW_Girl", 1020),
+                new Employee("Boss_Man", 1001),
+                new Employee("Boss_Woman", 1070),
+                new Employee("Office_Clown", 1060),
+                new Employee("Cat_Man", 1040)
+            };
             
             // UI //
             String userResponse = "";
@@ -46,26 +164,40 @@ namespace EmployeeProgram
                         break;
 
                     case "list":
-                        data.printArray();
+                        PrintArray();
                         break;
 
                     case "order":
-                        data.maxSort();
+                        Sort(true);
+                        Console.WriteLine("List sorted.");
                         break;
 
                     case "reverse_order":
-                        data.minSort();
+                        Sort(false);
+                        Console.WriteLine("List sorted reverse order.");
                         break;
 
                     case "reset":
-                        data.reset();
+                        Employee[] tempArray = {
+                        new Employee("Office_Lady", 1015),
+                        new Employee("Desk_Gentleman", 1050),
+                        new Employee("NEW_Girl", 1020),
+                        new Employee("Boss_Man", 1001),
+                        new Employee("Boss_Woman", 1070),
+                        new Employee("Office_Clown", 1060),
+                        new Employee("Cat_Man", 1040)
+            };
+
+                        EmployeeList = tempArray;
+                        Console.WriteLine("Employee list RESET complete.");
+                        Console.WriteLine("");
                         break;
 
                     case "end":
                         break;
 
                     default:
-                        Console.WriteLine("Command Unkown");
+                        Console.WriteLine("Command Unknown");
                         Console.WriteLine("");
                         break;
                 }
